0585c20ac3e6d5e8f2342e8a86e3cb87c265c111
Michael Bigelow <procureeminence@gmail.com>
2017-03-29 20:47:02 -0600

    Adding FileManager Class
    Created a new class called FileManager which will contain all file
    manipulation functionality.
    Added CopyDirectory method to the FileManager class.

---
+++ My Summer Backup/FileManager.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+
+namespace My_Summer_Backup
+{
+    class FileManager
+    {
+
+        public static bool OVERWRITE_FILES = true;
+        public static bool KEEP_FILES      = false;
+
+        public static void CopyDirectory( string sourceDirectoryName, string destinationDirectoryName, bool copySubdirectories, bool overwriteFiles )
+        {
+            DirectoryInfo sourceDirectory = new DirectoryInfo( sourceDirectoryName );
+
+            // Check if the source directory exists.
+            if( !sourceDirectory.Exists )
+            {
+                return;
+            }
+
+            // Check if the destination directory exists. If not, create it.
+            if( !Directory.Exists( destinationDirectoryName ) )
+            {
+                Directory.CreateDirectory( destinationDirectoryName );
+            }
+
+            // Get a list of all the files within the source directory.
+            FileInfo[] sourceDirectoryFiles = sourceDirectory.GetFiles();
+
+            // Copy all files to the destination directory.
+            foreach( FileInfo file in sourceDirectoryFiles )
+            {
+                string destinationPath = Path.Combine( destinationDirectoryName, file.Name );
+                file.CopyTo( destinationPath, overwriteFiles );
+            }
+
+            // Get a list of all subdirectories
+            DirectoryInfo[] sourceSubdirectories = sourceDirectory.GetDirectories();
+
+            // Copy source subdirectories to the destination directory if desired.
+            if( copySubdirectories )
+            {
+                foreach ( DirectoryInfo subdirectory in sourceSubdirectories )
+                {
+                    string destinationPath = Path.Combine( destinationDirectoryName, subdirectory.Name );
+                    CopyDirectory( subdirectory.FullName, destinationPath, copySubdirectories, OVERWRITE_FILES );
+                }
+            }
+        }
+    }
+}
