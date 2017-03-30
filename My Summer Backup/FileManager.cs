e8d6cee1f2ce5b296332891415314bd9dbf41cd8
Michael Bigelow <procureeminence@gmail.com>
2017-03-30 14:03:32 -0600

    Initial Backup Code Functioning
    The button used to create backups is now functioning. Keep in mind, this
    function doesn't read the backup file name field yet and it only copies
    the game data to a hard-coded location.

--- My Summer Backup/FileManager.cs
+++ My Summer Backup/FileManager.cs
@@ -1,51 +1,74 @@
 ﻿using System.IO;
+using System;
 
-namespace My_Summer_Backup
-{
-    class FileManager
-    {
+namespace My_Summer_Backup{
+
+    class FileManager{
 
         public static bool OVERWRITE_FILES = true;
         public static bool KEEP_FILES      = false;
+        public static bool COPY_SUBDIRS    = true;
+        public static bool LEAVE_SUBDIRS   = false;
+        public static string gameDataDirectory = @"%USERPROFILE%\AppData\LocalLow\Amistech\My Summer Car";
+        public static string backupDirectory   = @"%USERPROFILE%\Desktop\MySummerCarBackups\";
 
-        public static void CopyDirectory( string sourceDirectoryName, string destinationDirectoryName, bool copySubdirectories, bool overwriteFiles )
-        {
+        // Used to expand environment variables within a path. EX: %USERPROFILE%
+        public static string ExpandPath( string initialPath ){
+
+            string fullPath = Environment.ExpandEnvironmentVariables( initialPath );
+            return fullPath;
+
+        }
+
+        public static void CopyDirectory( string sourceDirectoryName, string destinationDirectoryName, bool copySubdirectories, bool overwriteFiles ){
+
+            sourceDirectoryName      = ExpandPath( sourceDirectoryName  );
+            destinationDirectoryName = ExpandPath( destinationDirectoryName );
+
             DirectoryInfo sourceDirectory = new DirectoryInfo( sourceDirectoryName );
 
             // Check if the source directory exists.
-            if( !sourceDirectory.Exists )
-            {
+            if( !sourceDirectory.Exists ){
+
                 return;
+
             }
 
             // Check if the destination directory exists. If not, create it.
-            if( !Directory.Exists( destinationDirectoryName ) )
-            {
+            if( !Directory.Exists( destinationDirectoryName ) ){
+
                 Directory.CreateDirectory( destinationDirectoryName );
+
             }
 
             // Get a list of all the files within the source directory.
             FileInfo[] sourceDirectoryFiles = sourceDirectory.GetFiles();
 
             // Copy all files to the destination directory.
-            foreach( FileInfo file in sourceDirectoryFiles )
-            {
+            foreach( FileInfo file in sourceDirectoryFiles ){
+
                 string destinationPath = Path.Combine( destinationDirectoryName, file.Name );
                 file.CopyTo( destinationPath, overwriteFiles );
+
             }
 
             // Get a list of all subdirectories
             DirectoryInfo[] sourceSubdirectories = sourceDirectory.GetDirectories();
 
             // Copy source subdirectories to the destination directory if desired.
-            if( copySubdirectories )
-            {
-                foreach ( DirectoryInfo subdirectory in sourceSubdirectories )
-                {
+            if( copySubdirectories ){
+
+                foreach ( DirectoryInfo subdirectory in sourceSubdirectories ){
+
                     string destinationPath = Path.Combine( destinationDirectoryName, subdirectory.Name );
                     CopyDirectory( subdirectory.FullName, destinationPath, copySubdirectories, OVERWRITE_FILES );
+
                 }
+
             }
+
         }
+
     }
+
 }
--- My Summer Backup/MainWindow.xaml.cs
+++ My Summer Backup/MainWindow.xaml.cs
@@ -13,16 +13,24 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace My_Summer_Backup
-{
+namespace My_Summer_Backup{
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window{
+
+        public MainWindow(){
+
             InitializeComponent();
+
+        }
+
+        private void createBackup( object sender, RoutedEventArgs e ) {
+
+            FileManager.CopyDirectory( FileManager.gameDataDirectory, FileManager.backupDirectory, FileManager.COPY_SUBDIRS, FileManager.OVERWRITE_FILES );
+
         }
     }
+
 }
